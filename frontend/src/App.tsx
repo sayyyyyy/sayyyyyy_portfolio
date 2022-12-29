53a35edc8ec151a9886bfce00ff59f8e098aee43
sayyyyyy <d958956a6b650@gmail.com>
2022-12-29 15:45:41 +0900

    headerComponentレイアウトの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 // Componentの読み込み
 import { HeaderComponent } from '../src/components/headerComponent'
+import { HomeComponent  } from './components/homeComponent';
 
 function App() {
   return (
     <div className="App">
       <p className='underline'>hello world</p>
       <HeaderComponent></HeaderComponent>
+      <HomeComponent></HomeComponent>
     </div>
   );
 }
---
+++ frontend/src/components/homeComponent.tsx
@@ -0,0 +1,10 @@
+
+export const HomeComponent = () => {
+    return (
+        <div>
+            <img alt="usericon" src="sayy.png" className="w-64"></img>
+            <p>I'm sayyyyyy</p>
+            <p>WEB ENGINEER</p>
+        </div>
+    );
+}
