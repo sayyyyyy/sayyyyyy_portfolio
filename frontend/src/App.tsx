7c0e641b9ffe7929ad0619b69abdcdcf6ee94076
sayyyyyy <d958956a6b650@gmail.com>
2022-12-29 15:41:46 +0900

    headerComponentレイアウトの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Componentの読み込み
+import { HeaderComponent } from '../src/components/headerComponent'
 
 function App() {
   return (
     <div className="App">
       <p className='underline'>hello world</p>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <HeaderComponent></HeaderComponent>
     </div>
   );
 }
---
+++ frontend/src/components/headerComponent.tsx
@@ -0,0 +1,12 @@
+
+export const HeaderComponent = () => {
+    return (
+        <header className="flex">
+            <p>SKILL</p>
+            <p>WORK</p>
+            <img alt="usericon" src="sayy.png" className="w-20"></img>
+            <p>HISTORY</p>
+            <p>SNS</p>
+        </header>
+    );
+}
