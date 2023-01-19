36c7e04fa3e0306f2a7c2d0cb36884c4324964fb
sayyyyyy <d958956a6b650@gmail.com>
2023-01-19 23:55:33 +0900

    feat: historyの追加

--- src/pages/historyAllPage.tsx
+++ src/pages/historyAllPage.tsx
@@ -8,7 +8,7 @@ export const HistoryAllPage = () => {
         {historyAge: "2021.03", historyContent: "プログラミングを始める", historyDescription: "技育祭に参加したことをきっかけにプログラミングを始める"},
         {historyAge: "2021.03", historyContent: "ハッカソン初参加", historyDescription: "技育祭でハッカソンの存在を知り参加する。このハッカソンの後からガッツリプログラミングをするようになる"},
         {historyAge: "2021.05", historyContent: "CODEGYM Academy受講", historyDescription: "周りのエンジニア学生との差を知り、技術を身につけるために受講する"},
-        {historyAge: "2021.10", historyContent: "HackU Meijo参加", historyDescription: "CODEGYM Academyの受講が終修了し、力試しとして参加。SpaceShuttleというアプリを開発"},
+        {historyAge: "2021.10", historyContent: "HackU Meijo参加", historyDescription: "CODEGYM Academyの受講が修了し、力試しとして参加。SpaceShuttleというアプリを開発"},
         {historyAge: "2021.11", historyContent: "長期インターン開始", historyDescription: "より多くのスキルを得るためにフルスタックエンジニアとして長期インターンを開始。設計、ネットワーク、Chromeの拡張機能、Webフロントエンド、バックエンドなどの幅広いことを担当"}
     ]
 
@@ -22,12 +22,16 @@ export const HistoryAllPage = () => {
         {historyAge: "2022.11", historyContent: "XRCreativeAward参加", historyDescription: "Fillinというスマホアプリを開発"},
         {historyAge: "2022.11", historyContent: "技育CAMPハッカソンvol8　努力賞受賞", historyDescription: "GetoryというWebアプリを開発する。初めて1人でハッカソンに参加し、努力賞を受賞"},
         {historyAge: "2022.12", historyContent: "HackU Meijo参加", historyDescription: "つなぐまというWebアプリを開発"},
-        {historyAge: "2022.12", historyContent: "WithARハッカソン参加", historyDescription: "areal-Airrealというスマホアプリを開発"}
+        {historyAge: "2022.12", historyContent: "WithARハッカソン参加", historyDescription: "areal-Airrealというスマホアプリを開発"},
     ]
 
-    const history2023 = []
+    const history2023 = [
+        {historyAge: "2023.01", historyContent: "Webサービス開発者の会 登壇", historyDescription: "さくらのマイクロコミュニティ （Webサービス開発者の会） #5にて登壇"},
+        {historyAge: "2023.01", historyContent: "インプット・アウトプットどうしてる? 登壇", historyDescription: "【LT会】インプット・アウトプットどうしてる? in 名古屋にて登壇"},
+        {historyAge: "2023.01", historyContent: "長期インターン始めようぜ会　企画", historyDescription: "東海圏の学生エンジニア向けに長期インターン始めようぜ会を企画、運営全て1人で担当"},
+    ]
 
-    const historyAll = [...history2021, ...history2022]
+    const historyAll = [...history2021, ...history2022, ...history2023]
     return (
         <div className="relative top-24">
             <TitleComponent>History All</TitleComponent>
