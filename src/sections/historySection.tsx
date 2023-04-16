f97c23d95ba6ab174fa0461848aa77d525f3d44f
sayyyyyy <d958956a6b650@gmail.com>
2023-04-16 20:14:42 +0900

    fix: すべての処理をスプレッドシートからの取得に変更

--- src/sections/historySection.tsx
+++ src/sections/historySection.tsx
@@ -4,10 +4,12 @@ import { HistoryElement } from "../components/historyElement";
 import "../components/history.css"
 
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSpreadSheetData } from "../scripts/getSpreadSheetData";
 
 export const HistorySection = () => {
     const [openMenu, setOpenMenu] = useState(false);
+    const [historyList, setHistory] = useState([])
     
     const navigate = useNavigate()
     const navigateHistoryAll = () => {
@@ -16,24 +18,28 @@ export const HistorySection = () => {
         window.scrollTo(0, 0)
     }
 
-    const historyList = [
-        {historyAge: "2021.03", historyContent: "プログラミングを始める", historyDescription: "技育祭に参加したことをきっかけにプログラミングを始める"},
-        {historyAge: "2021.03", historyContent: "ハッカソン初参加", historyDescription: "技育祭でハッカソンの存在を知り参加する。このハッカソンの後からガッツリプログラミングをするようになる"},
-        {historyAge: "2021.05", historyContent: "CODEGYM Academy受講", historyDescription: "周りのエンジニア学生との差を知り、技術を身につけるために受講する"},
-        {historyAge: "2021.11", historyContent: "長期インターン開始", historyDescription: "より多くのスキルを得るためにフルスタックエンジニアとして長期インターンを開始。設計、ネットワーク、Chromeの拡張機能、Webフロントエンド、バックエンドなどの幅広いことを担当"},
-        {historyAge: "2022.04", historyContent: "学生団体Idea×Techに運営兼メンターとして加入", historyDescription: "友人に誘われ、エンジニア学生団体であるIdea×TechにWebメンター兼運営として加入。団体の運営や企画などを担当"},
-        {historyAge: "2022.05", historyContent: "技育CAMPハッカソンvol2参加　努力賞受賞", historyDescription: "MuscleOnlineというWebアプリを開発し、努力賞を受賞"},
-        {historyAge: "2022.06", historyContent: "CODEGYM Academy先輩チューター業務", historyDescription: "CODEGYM Academyで先輩チューター業務を行う。人に教える難しさを知り、また自分自身が人に教えることが好きだと気付く"},
-        {historyAge: "2022.09", historyContent: "技育展・CCCu-22参加　CCC-u22にてファイナリスト選出、企業賞受賞", historyDescription: "以前制作したMuscleOnlineをUnityで再制作。CCCu-22と技育展に出場し、CCCu-22ではファイナリストに選出され、企業賞を受賞"},
-        {historyAge: "2022.11", historyContent: "技育CAMPハッカソンvol8　努力賞受賞", historyDescription: "GetoryというWebアプリを開発する。初めて1人でハッカソンに参加し、努力賞を受賞"},
-    ]
+    useEffect(() => {
+        const request = {
+            params: {
+                operation: 'history'
+            }
+        }
+
+        const getHistoryData = async () => {
+            setHistory(await getSpreadSheetData(request))
+        }
+        
+        getHistoryData()
+    }, []);
+
+
     return (
         <section id="history-container" className="mt-24">
             <TitleComponent>HISTORY</TitleComponent>
             <dl className="flex flex-wrap justify-between mt-6">
                 {historyList.map((history) => {
                     return (
-                        <HistoryElement key={history.historyDescription} historyAge={history.historyAge} historyDescription={history.historyDescription}>{history.historyContent}</HistoryElement>
+                        <HistoryElement key={history["historyDescription"]} historyAge={history["historyAge"]} historyDescription={history["historyDescription"]}>{history["historyContent"]}</HistoryElement>
                     );
                 })}
             </dl>
--- src/sections/workSection.tsx
+++ src/sections/workSection.tsx
@@ -2,9 +2,12 @@ import { WorkElement } from "../components/workElement";
 import { TitleComponent } from "../components/titleComponent";
 
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSpreadSheetData } from "../scripts/getSpreadSheetData";
 
 export const WorkSection = () => {
+    const [workList, setWorkList] = useState([])
+
     type WorkType = {
         workSrc: string;
         workTitle: string
@@ -18,68 +21,19 @@ export const WorkSection = () => {
         workResponsibilty: string
     }
 
-    const workList = [
-        {
-            workSrc: "images/works/KnowledgeLibrary.png", 
-            workTitle: "Knowledge Library", 
-            workOverview: "ブックマークの拡張版を目指したWebアプリ", 
-            workObsession: "極力手間を省くように、URLを入力したらスクレイピングでタイトルを自動で取得できるようにしました。", 
-            workSkill: ["HTML", "CSS", "JavaScript", "Python", "Flask", "SQLite"], 
-            workPeriod: "1ヶ月", 
-            workLink: "https://knowledge-library.azurewebsites.net/", 
-            githubLink: "https://github.com/sayyyyyy/knowledge_library", 
-            workNumOfPeople: 1, 
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/Sugures.png", 
-            workTitle: "すぐレス", 
-            workOverview: "様々な条件から最寄りの飲食店を検索できます。", 
-            workObsession: "競合サービスがいくつかあるためシンプルさを売りにし、できるだけ簡単に探せるようにしました。食欲増進のためのオレンジをメインカラーとしてとして利用しました。", 
-            workSkill: ["HTML", "CSS", "JavaScript", "Python", "Flask", "ホットペッパーグルメリサーチAPI", "Heroku"], 
-            workPeriod: "2週間", 
-            workLink: "https://sugures.herokuapp.com/", 
-            githubLink: "https://github.com/sayyyyyy/sugures", 
-            workNumOfPeople: 1,
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/MuscleOnlineUnity.png", 
-            workTitle: "Muscle Online Unity版", 
-            workOverview: "他のユーザーと楽しく筋トレを行うサービス", 
-            workObsession: "スマホ1台で楽しむことが可能で、1人ではなく様々な人と競いながら楽しく筋トレを行うことができる。", 
-            workSkill: ["Unity", "FireStore", "Figma", "Adobe Photoshop", "Cascadeur", "blender", "Cubase"], 
-            workPeriod: "3ヶ月", 
-            workLink: "", 
-            githubLink: "https://github.com/sayyyyyy/MuscleOnline-ccc2022", 
-            workNumOfPeople: 6,
-            workResponsibilty: "Unityでの機能作成、データベース処理、Photonを利用したマルチゲーム化"
-        },
-        {
-            workSrc: "images/works/Getory.png", 
-            workTitle: "Getory", 
-            workOverview: "言語ごとのリポジトリやランダムなリポジトリを簡単に発見できるサービス", 
-            workObsession: "様々な人に使ってもらえるように多言語対応、ダークモード切り替えを実装", 
-            workSkill: ["TailwindCSS", "TypeScript", "Vue.js", "Nuxt.js", "docker", "GitHub API", "Netlify"], 
-            workPeriod: "1週間", 
-            workLink: "https://getory.netlify.app/", 
-            githubLink: "https://github.com/sayyyyyy/Getory", 
-            workNumOfPeople: 1,
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/arealAirreal.png", 
-            workTitle: "areal-AirReal", 
-            workOverview: "空中に絵を描いて写真にできるサービス。他の人のも見ることができる。", 
-            workObsession: "", 
-            workSkill: ["Unity", "FireStore", "Firebase Storage", "Adobe XD", "Geospatial API"], 
-            workPeriod: "2週間", 
-            workLink: "", 
-            githubLink: "", 
-            workNumOfPeople: 6,
-            workResponsibilty: "写真データの保存から表示"
-        },
-    ]
+    useEffect(() => {
+        const request = {
+            params: {
+                operation: 'work'
+            }
+        }
+
+        const getWorkData = async () => {
+            setWorkList(await getSpreadSheetData(request))
+        }
+        
+        getWorkData()
+    }, []);
 
     const [openMenu, setOpenMenu] = useState(false);
 
