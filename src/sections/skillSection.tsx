baae875de41a2a376e494bd5a1f92d2c8134a679
sayyyyyy <d958956a6b650@gmail.com>
2026-03-14 09:28:01 +0900

    feat: 要素をハードコードではなくjsonから読み取るように修正

--- src/sections/skillSection.tsx
+++ src/sections/skillSection.tsx
@@ -1,83 +1,52 @@
 import { TitleComponent } from "../components/titleComponent";
 import { SkillElement } from "../components/skillElement";
+import skillData from '../data/skillData.json';
 
 export const SkillSection = () => {
-    const webFrontendSkillList = [
-        {skillName: "HTML"},
-        {skillName: "CSS"},
-        {skillName: "Sass"},
-        {skillName: "Tailwind CSS"},
-        {skillName: "JavaScript"},
-        {skillName: "TypeScript", skillSrc: "images/skills/typescript.png"},
-        {skillName: "Vue.js"},
-        {skillName: "Nuxt.js"},
-        {skillName: "React"},
-        {skillName: "Angular.js"},
-    ]
-    const webBackEndSkillList = [
-        {skillName: "Python"},
-        {skillName: "Flask"},
-        {skillName: "PHP"},
-        {skillName: "C#"},
-    ]
-    const gameSkillList = [
-        {skillName: "Unity"},
-    ]
+    const { webFrontendSkillList, webBackEndSkillList, gameSkillList, mobileSkillList, otherSkillList } = skillData;
 
-    const mobileSkillList = [
-        {skillName: "Dart"},
-        {skillName: "Flutter"},
-    ]
-
-    const otherSkillList = [
-        {skillName: "C"},
-        {skillName: "Git"},
-        {skillName: "Docker"},
-        {skillName: "Solidity"},
-    ]
-
-    return(
+    return (
         <div id="skill-container" className="w-screen">
             <TitleComponent>SKILL</TitleComponent>
             <h3 className="text-3xl m-8">WebFrontEnd</h3>
             <div className="flex flex-wrap m-8 border-2 rounded-md">
-            {webFrontendSkillList.map((skill) => {
-                return (
-                    <SkillElement key={skill.skillName} skillName={skill.skillName}/>
-                );
-            })}
+                {webFrontendSkillList.map((skill) => {
+                    return (
+                        <SkillElement key={skill.skillName} skillName={skill.skillName} />
+                    );
+                })}
             </div>
             <h3 className="text-3xl m-8">WebBackEnd</h3>
             <div className="flex flex-wrap m-8 border-2 rounded-xl">
-            {webBackEndSkillList.map((skill) => {
-                return (
-                    <SkillElement key={skill.skillName} skillName={skill.skillName}/>
-                );
-            })}
+                {webBackEndSkillList.map((skill) => {
+                    return (
+                        <SkillElement key={skill.skillName} skillName={skill.skillName} />
+                    );
+                })}
             </div>
             <h3 className="text-3xl m-8">Game</h3>
             <div className="flex flex-wrap m-8 border-2 rounded-xl">
-            {gameSkillList.map((skill) => {
-                return (
-                    <SkillElement key={skill.skillName} skillName={skill.skillName}/>
-                );
-            })}
+                {gameSkillList.map((skill) => {
+                    return (
+                        <SkillElement key={skill.skillName} skillName={skill.skillName} />
+                    );
+                })}
             </div>
             <h3 className="text-3xl m-8">Mobile</h3>
             <div className="flex flex-wrap m-8 border-2 rounded-xl">
-            {mobileSkillList.map((skill) => {
-                return (
-                    <SkillElement key={skill.skillName} skillName={skill.skillName}/>
-                );
-            })}
+                {mobileSkillList.map((skill) => {
+                    return (
+                        <SkillElement key={skill.skillName} skillName={skill.skillName} />
+                    );
+                })}
             </div>
             <h3 className="text-3xl m-8">Others</h3>
             <div className="flex flex-wrap m-8 border-2 rounded-xl">
-            {otherSkillList.map((skill) => {
-                return (
-                    <SkillElement key={skill.skillName} skillName={skill.skillName}/>
-                );
-            })}
+                {otherSkillList.map((skill) => {
+                    return (
+                        <SkillElement key={skill.skillName} skillName={skill.skillName} />
+                    );
+                })}
             </div>
         </div>
     );
--- src/sections/workSection.tsx
+++ src/sections/workSection.tsx
@@ -3,84 +3,21 @@ import { TitleComponent } from "../components/titleComponent";
 
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import workList from '../data/workData.json';
 
 export const WorkSection = () => {
     type WorkType = {
         workSrc: string;
         workTitle: string
         workOverview: string;
-        workObsession: string; 
+        workObsession: string;
         workSkill: Array<string>;
-        workPeriod: string, 
-        workLink: string, 
-        githubLink: string, 
+        workPeriod: string,
+        workLink: string,
+        githubLink: string,
         workNumOfPeople: number,
         workResponsibilty: string
     }
-
-    const workList = [
-        {
-            workSrc: "images/works/KnowledgeLibrary.png", 
-            workTitle: "Knowledge Library", 
-            workOverview: "ブックマークの拡張版を目指したWebアプリ", 
-            workObsession: "極力手間を省くように、URLを入力したらスクレイピングでタイトルを自動で取得できるようにしました。", 
-            workSkill: ["HTML", "CSS", "JavaScript", "Python", "Flask", "SQLite"], 
-            workPeriod: "1ヶ月", 
-            workLink: "https://knowledge-library.azurewebsites.net/", 
-            githubLink: "https://github.com/sayyyyyy/knowledge_library", 
-            workNumOfPeople: 1, 
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/Sugures.png", 
-            workTitle: "すぐレス", 
-            workOverview: "様々な条件から最寄りの飲食店を検索できます。", 
-            workObsession: "競合サービスがいくつかあるためシンプルさを売りにし、できるだけ簡単に探せるようにしました。食欲増進のためのオレンジをメインカラーとしてとして利用しました。", 
-            workSkill: ["HTML", "CSS", "JavaScript", "Python", "Flask", "ホットペッパーグルメリサーチAPI", "Heroku"], 
-            workPeriod: "2週間", 
-            workLink: "https://sugures.herokuapp.com/", 
-            githubLink: "https://github.com/sayyyyyy/sugures", 
-            workNumOfPeople: 1,
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/MuscleOnlineUnity.png", 
-            workTitle: "Muscle Online Unity版", 
-            workOverview: "他のユーザーと楽しく筋トレを行うサービス", 
-            workObsession: "スマホ1台で楽しむことが可能で、1人ではなく様々な人と競いながら楽しく筋トレを行うことができる。", 
-            workSkill: ["Unity", "FireStore", "Figma", "Adobe Photoshop", "Cascadeur", "blender", "Cubase"], 
-            workPeriod: "3ヶ月", 
-            workLink: "", 
-            githubLink: "https://github.com/sayyyyyy/MuscleOnline-ccc2022", 
-            workNumOfPeople: 6,
-            workResponsibilty: "Unityでの機能作成、データベース処理、Photonを利用したマルチゲーム化"
-        },
-        {
-            workSrc: "images/works/Getory.png", 
-            workTitle: "Getory", 
-            workOverview: "言語ごとのリポジトリやランダムなリポジトリを簡単に発見できるサービス", 
-            workObsession: "様々な人に使ってもらえるように多言語対応、ダークモード切り替えを実装", 
-            workSkill: ["TailwindCSS", "TypeScript", "Vue.js", "Nuxt.js", "docker", "GitHub API", "Netlify"], 
-            workPeriod: "1週間", 
-            workLink: "https://getory.netlify.app/", 
-            githubLink: "https://github.com/sayyyyyy/Getory", 
-            workNumOfPeople: 1,
-            workResponsibilty: "すべて"
-        },
-        {
-            workSrc: "images/works/arealAirreal.png", 
-            workTitle: "areal-AirReal", 
-            workOverview: "空中に絵を描いて写真にできるサービス。他の人のも見ることができる。", 
-            workObsession: "", 
-            workSkill: ["Unity", "FireStore", "Firebase Storage", "Adobe XD", "Geospatial API"], 
-            workPeriod: "2週間", 
-            workLink: "", 
-            githubLink: "", 
-            workNumOfPeople: 6,
-            workResponsibilty: "写真データの保存から表示"
-        },
-    ]
-
     const [openMenu, setOpenMenu] = useState(false);
 
     const navigate = useNavigate()
@@ -94,25 +31,25 @@ export const WorkSection = () => {
         <section id="work-container">
             <TitleComponent>WORK</TitleComponent>
             <div className="md:grid md:grid-cols-2 xl:grid-cols-3 mt-16">
-                {workList.map((work: WorkType) => {
+                {workList.filter((w) => w.isPickup).map((work: WorkType) => {
                     return (
                         <WorkElement
-                            key={work.workTitle} 
-                            workSrc={work.workSrc} 
-                            workTitle={work.workTitle} 
-                            workOverview={work.workOverview} 
-                            workObsession={work.workObsession} 
-                            workSkill={work.workSkill} 
-                            workPeriod={work.workPeriod} 
-                            workLink={work.workLink} 
-                            githubLink={work.githubLink} 
-                            workNumOfPeople={work.workNumOfPeople} 
-                            workResponsibilty={work.workResponsibilty} 
+                            key={work.workTitle}
+                            workSrc={work.workSrc}
+                            workTitle={work.workTitle}
+                            workOverview={work.workOverview}
+                            workObsession={work.workObsession}
+                            workSkill={work.workSkill}
+                            workPeriod={work.workPeriod}
+                            workLink={work.workLink}
+                            githubLink={work.githubLink}
+                            workNumOfPeople={work.workNumOfPeople}
+                            workResponsibilty={work.workResponsibilty}
                         />
                     );
                 })}
             </div>
-            <p onClick={() => {navigateWorkAll()}} className="flex justify-end mt-8 text-zinc-400 cursor-pointer">すべて見る ＞</p>
+            <p onClick={() => { navigateWorkAll() }} className="flex justify-end mt-8 text-zinc-400 cursor-pointer">すべて見る ＞</p>
         </section>
     );
 }
